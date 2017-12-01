35400d7bd89483ecdaf73e257ee3c75bfdac8ed9
LonelyRunner <826270362@qq.com>
2017-12-01 17:28:43 +0800

    [郑豪]:node基础学习之模块

---
+++ module/1.js
@@ -0,0 +1,22 @@
+//局部变量
+var a = 100;
+
+//全局变量
+global.a = 200;
+
+console.info("局部变量a="+a);
+console.info("全局变量a="+global.a);
+
+//javascript中的全局对象是window  nodejs中的全局变量是global
+
+//__filename:输出当前文件的绝对路径
+console.info(__filename);
+
+//require方法
+require('./2.js');
+
+//模块加载机制
+/*
+    1.路径：相对路径和 绝对路径
+    2.加载顺序： 1.文件名称 -> +.js -> +.json -> +.node -> 报错
+ */
---
+++ module/2.js
@@ -0,0 +1,1 @@
+console.info("这是2.js文件");
